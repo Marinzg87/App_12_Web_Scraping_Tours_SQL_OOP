d415a00b0e43849047415d1917658862794f6990
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-04-14 23:10:57 +0200

    Create two new classes. Class Event and Email.

--- main.py
+++ main.py
@@ -1,61 +1,65 @@
 import requests
 import selectorlib
-import smtplib, ssl
+import smtplib
+import ssl
 import os
 import time
 import sqlite3
 
-"INSERT INTO events VALUES ('Tigers', 'Tiger City', '2088.10.14')"
-"SELECT * FROM events WHERE date='2088.10.15'"
+PASSWORD = os.getenv("GMAIL_WEBCAM_MESSAGE")
+SENDER = os.getenv("GMAIL_EMAIL_ADDRESS")
+RECEIVER = os.getenv("GMAIL_EMAIL_ADDRESS")
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                  '/AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/39.0.2171.95 /Safari/537.36'}
 
 connection = sqlite3.connect("data.db")
 
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from the URL"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Email:
+    def send(self, message):
+        host = "smtp.gmail.com"
+        port = 465
+
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(SENDER, PASSWORD)
+            server.sendmail(SENDER, RECEIVER, message)
+        print("Email was sent!")
 
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
-
-    username = "app8flask@gmail.com"
-    password = "qyciukmocfaiarse"
-
-    receiver = "app8flask@gmail.com"
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Email was sent!")
-
-
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
+def store(extracted_local):
+    row_local = extracted_local.split(",")
+    row_local = [item.strip() for item in row_local]
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row_local)
     connection.commit()
 
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
+
+def read(extracted_local):
+    row_local = extracted_local.split(",")
+    row_local = [item.strip() for item in row_local]
+    band, city, date = row_local
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND "
+                   "date=?", (band, city, date))
     rows = cursor.fetchall()
     print(rows)
     return rows
@@ -63,13 +67,15 @@ def read(extracted):
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
             row = read(extracted)
             if not row:
                 store(extracted)
-                send_email(message="Hey, new event was found!")
+                email = Email()
+                email.send(message="Hey, new event was found!")
         time.sleep(2)
